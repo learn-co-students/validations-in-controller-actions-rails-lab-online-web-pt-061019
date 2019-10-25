ee367c9d16d2e8ae95db9f7efcaf056c1dd3892a
HuydDo <huydinhdo@yahoo.com>
2019-10-25 12:53:12 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,8 +8,8 @@ class AuthorsController < ApplicationController
 
   def create
     # binding.pry
-    if @author = Author.create(author_params)
-    # if @author.save
+    @author = Author.create(author_params)
+    if @author.save
       redirect_to author_path(@author)
     else
       render :new
